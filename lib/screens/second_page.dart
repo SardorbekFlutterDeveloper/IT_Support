ec0fb1da3326ca89755dd2b94b5f6ef08e22af10
Sardor Sattorov <95919851+SardorbekFlutterDeveloper@users.noreply.github.com>
2023-06-15 17:36:31 +0500

    added ame changed to ui

---
+++ lib/screens/second_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeconsPage extends StatefulWidget {
+  const SeconsPage({super.key});
+
+  @override
+  State<SeconsPage> createState() => _SeconsPageState();
+}
+
+class _SeconsPageState extends State<SeconsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
